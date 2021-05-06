50f3a382a2087f2f023239e3049a73d8b281d5f5
lulongfei <lulongfei@of3d.com>
2021-05-06 18:35:02 +0800

    update sort algorithms codes

---
+++ Main.cpp
@@ -0,0 +1,221 @@
+// SortingAlgs.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <math.h>
+#include <chrono>
+#include <thread>
+//#include <unistd.h>
+#include <iostream>
+
+#include "Bar.h"
+
+#define LOG(x) std::cout << x << std::endl;
+
+bool running = true;
+
+void swap(int *a, int i, int j) {
+	int temp = a[i];
+	a[i] = a[j];
+	a[j] = temp;
+}
+
+void shuffle(int *a, int arraySize) {
+	srand(time(NULL));
+	for (int i = arraySize - 1; i > 0; --i) {
+		int j = rand() % i + 1;
+		swap(a, i, j);
+	}
+}
+
+void renderBars(Bar *bars[], int barsCount, sf::RenderWindow &window) {
+	for (int i = 0; i < barsCount; ++i) {
+		bars[i]->getRect()->setPosition(((window.getSize().x) / barsCount) * (i), window.getSize().y);
+	}
+	window.clear(sf::Color(120, 125, 135));
+	for (int i = 0; i < barsCount; ++i) {
+		window.draw(sf::RectangleShape(*(bars[i]->getRect())));
+	}
+
+	window.display();
+}
+
+void cpy_arr(Bar *src[], Bar *dest[], int start, int end) {
+	for (int i = start; i < end; ++i) {
+		dest[i - start] = new Bar(src[i]->getN());
+	}
+}
+
+void merge(int *arr, int l, int m, int r) {
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	std::vector<int> L, R;
+	for (i = 0; i < n1; i++) {
+		std::cout << l + i << std::endl;
+		L.push_back(arr[l + i]);
+	}
+
+	for (j = 0; j < n2; j++) {
+		R.push_back(arr[m + 1 + j]);
+	}
+
+	i = 0;
+	j = 0;
+	k = l;
+	while (i < n1 && j < n2) {
+		if (L[i] <= R.at(j)) {
+			//arr[k] = L[i];
+			arr[k] = L.at(i);
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) {
+		arr[k] = L[i];
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		arr[k] = R[j];
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+		j++;
+		k++;
+	}
+}
+
+void mergeSort(int *arr, int l, int r) {
+	if (!running)
+		return;
+	if (l < r) {
+		int m = l + (r - l) / 2;
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+		merge(arr, l, m, r);
+	}
+}
+
+int quickSortPartition(int *a, int lowI, int highI) {
+	int pivot = a[highI];
+	int i = lowI - 1;
+
+	for (int j = lowI; j <= highI - 1; j++) {
+		if (a[j] < pivot) {
+			if (!running)
+				return -1;
+			i++;
+			swap(a, i, j);
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+		}
+	}
+	swap(a, i + 1, highI);
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+	return i + 1;
+}
+
+void quickSort(int *numbers, int lowI, int highI) {
+	if (!running)
+		return;
+
+	if (lowI < highI) {
+		int pi = quickSortPartition(numbers, lowI, highI);
+		std::cout << pi << std::endl;
+		quickSort(numbers, lowI, pi - 1);
+		quickSort(numbers, pi + 1, highI);
+	}
+}
+
+void insertionSort(int *arr, int numCount) {
+	for (int i = 0; i < numCount; ++i) {
+		for (int j = i; j > 0 && arr[j - 1] > arr[j]; --j) {
+			if (!running)
+				return;
+			swap(arr, j, j - 1);
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+		}
+	}
+}
+
+
+void bubbleSort(int *arr, int numCount) {
+	bool swapped;
+	do {
+		swapped = false;
+		for (int i = 1; i < numCount; ++i) {
+			if (!running)
+				return;
+			if (arr[i - 1] > arr[i]) {
+				swap(arr, i, i - 1);
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			}
+		}
+	} while (!swapped);
+}
+
+void updateBars(Bar *bars[], int *numbers, int numCount) {
+	for (int i = 0; i < numCount; ++i) {
+		bars[i]->setN(numbers[i]);
+	}
+}
+
+int main() {
+	const int numCount = 50;
+
+	Bar *bars[numCount];
+	int numbers[numCount];
+
+	sf::RenderWindow window(sf::VideoMode(960, 540), "Sorting Algorithms", sf::Style::Close);
+	int dx = window.getSize().x / numCount;
+	int dy = window.getSize().y / numCount;
+
+	for (int i = 0; i < numCount; ++i) {
+		bars[i] = new Bar(dx, i, dy);
+		numbers[i] = i;
+	}
+
+	shuffle(numbers, numCount);
+	updateBars(bars, numbers, numCount);
+
+	for (int i = 0; i < numCount; ++i) {
+		bars[i]->getRect()->setPosition(dx*i, window.getSize().y);
+	}
+
+	renderBars(bars, numCount, window);
+	std::thread sort(&bubbleSort, numbers, numCount);
+	//std::thread sort(&quickSort, numbers, 0, numCount - 1);
+	//std::thread sort(&mergeSort, numbers, 0, numCount - 1);
+	//std::thread sort(&insertionSort, numbers, numCount);
+
+	sf::Event event;
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		updateBars(bars, numbers, numCount);
+		renderBars(bars, numCount, window);
+	}
+	running = false;
+	for (int i = 0; i < numCount; ++i) {
+		delete bars[i];
+	}
+
+	sort.join();
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+##Sort Algorithms
+- bubbleSort
+![bubbleSort](https://github.com/chuckbruno/SortAlgorithms/blob/main/res/bubbleSort.gif)
+- quickSort
+![quickSort](https://github.com/chuckbruno/SortAlgorithms/blob/main/res/quickSort.gif)
+- mergeSort
+![mergeSort](https://github.com/chuckbruno/SortAlgorithms/blob/main/res/mergeSort.gif)
+- insertionSort
+![insertionSort](https://github.com/chuckbruno/SortAlgorithms/blob/main/res/insertionSort.gif)
